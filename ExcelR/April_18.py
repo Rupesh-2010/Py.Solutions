4c2acd18282a84b208cd4c7ac6f754fd9c642698
Rupesh-2010 <rups1808@gmail.com>
2025-04-21 21:10:46 +0530

    Funcion.

---
+++ ExcelR/April_18.py
@@ -0,0 +1,67 @@
+"""_________________________________Funtion_________________________________
+
+In Python, a function is a block of code that performs a specific task.
+You define a function once and can use (or call) it many times.
+
+
+def function_name():
+    body of code.
+
+#########
+def ---> Keyword
+function_name() ----> name of the function.
+
+"""
+#Method 1
+
+# def Msg():
+#     print("Good Morning.")
+
+# Msg()
+
+############
+
+#Method 2 --> Funcion argeument.
+
+# def Msg(name):
+#     print("Good Morning.",name)
+
+# Msg("Rupesh")
+"""_________________"""
+# Name --> parameter---->A veriable which is passed inside the function.
+# Rupesh --> Argrument----> value given to the parameter, it is called Argrument.
+"""+++++++++++++++++++++++"""
+
+
+# def Add(Num1,Num2):
+#     Result = Num1+Num2
+#     print(Result)
+# Add(100,200)
+
+
+"""Method 3 --> Return type
+    -it denotes that the function has ended. any code return is not excuted.
+"""
+
+# def addI(Num1, Num2):
+#     return Num1+Num2
+# print("wow")
+
+# A =addI(100,200)
+# print(A)
+
+"""lambda argrumnet expression.
+
+lambda arg(): expression
+arguments --> any value passed to the lambda
+expression --> expression is excuted and return.
+
+"""
+
+
+
+
+
+
+
+
