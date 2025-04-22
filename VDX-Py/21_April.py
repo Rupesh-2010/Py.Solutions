d9cb422884e25d51593834430deb51559bfa4ce2
Rupesh-2010 <rups1808@gmail.com>
2025-04-22 23:09:08 +0530

    .

--- VDX-Py/21_April.py
+++ VDX-Py/21_April.py
@@ -39,5 +39,85 @@ S2 = {5,6,7,1,8,9,10}
 
 
 
+"""On some basic cell phones, text messages can be sent using the numeric keypad.
+Because each key has multiple letters associated with it,
+multiple key presses are needed for most letters. Pressing the number once generates
+the first letter on the key. Pressing the number 2, 3, 4 or 5 times generates the second,
+third, fourth or fifth character listed for that key.
+
+Key		Symbols
+1		.,?!:
+2		ABC
+3		DEF
+4		GHI
+5		JKL
+6		MNO
+7		PQRS
+8		TUV
+9		WXYZ
+0		space
+
+Write a program that displays the key presses that must be made to enter a text message
+read from the user. Construct a dictionary that maps from each letter or symbol to the
+key presses. Then use the dictionary to generate and display the presses for the user's message.
+For example, if the user enters Hello, World! then your program should
+output 4433555555666110966677755531111. Ensure that your program handles both
+uppercase and lowercase letters. Ignore any characters that aren't listed in the
+table above such as semicolons and brackets."""
+
+K_Ph = {
+    "1" : ".",
+    "11" : ",",
+    "111" : "?",
+    "1111" : "!",
+    "11111" : ":",
+    "2" : "A",
+    "22" : "B",
+    "222" : "C",
+    "3" :"D",
+    "33" :"E",
+    "333" :"F",
+    "4" : "G",
+    "44" : "H",
+    "444" : "I",
+    "5" : "J",
+    "55" : "K",
+    "555" : "L",
+    "6" : "M",
+    "66" : "N",
+    "666" : "O",
+    "7" : "P",
+    "77" : "Q",
+    "777" : "R",
+    "7777" : "S",
+    "8" : "T",
+    "88" : "U",
+    "888" : "V",
+    "9" : "W",
+    "99" : "X",
+    "999" : "Y",
+    "9999" : "Z",
+    "0" : " "
+}
+
+msg = input("Enter Your msg: ").upper()
+result = ""
+
+for char in msg:
+    found = False
+    for key, value in K_Ph.items():
+        if value == char:
+            result += key
+            found = True
+            break
+    if not found:
+        pass
+print("Key presses:", result)
+
+
+
+
+
+
 
 
--- VDX-Py/22_April.py
+++ VDX-Py/22_April.py
@@ -6,3 +6,118 @@ You can reuse it anytime by calling its name.
 To organize your code
 To avoid repeating code
 To make your code clean and easy to read"""
+
+# def sum(A,B):
+#     sum = A+B
+#     print(sum)
+#     return(sum)
+
+# sum(10,20)
+# sum(100,20)
+# sum(1087,20)
+# sum(10,7620)
+
+####################
+
+# def square(A):
+#     square= A*A
+#     print(square)
+
+# square(4)
+
+####################
+
+# def is_even(num):
+#     if num%2==0:
+#         print(True)
+#     else:
+#         print(False)
+
+# is_even(21)
+
+##################
+
+# def L1(LL):
+#     Mx = max(LL)
+#     print(Mx)
+# L1(LL=[1,2,3,4,5])
+
+##############//////////////////
+
+# balance = 1000
+
+# def deposit():
+#     global balance
+#     amount = int(input("Enter amount to deposit: "))
+#     balance += amount
+#     print("Amount deposited successfully.")
+#     print("Current Balance:", balance)
+
+# def withdraw():
+#     global balance
+#     amount = int(input("Enter amount to withdraw: "))
+#     if amount <= balance:
+#         balance -= amount
+#         print("Amount withdrawn successfully.")
+#     else:
+#         print("Insufficient balance.")
+#     print("Current Balance:", balance)
+
+# def main():
+#     print("1. Deposit")
+#     print("2. Withdraw")
+#     choice = input("Enter your choice (1 or 2): ")
+    
+#     if choice == '1':
+#         deposit()
+#     elif choice == '2':
+#         withdraw()
+#     else:
+#         print("Invalid choice.")
+
+# main()
+
+
+##################################
+
+
+# def result(*sub):
+#     print("marks", sub)
+#     ave = sum(sub)/len(sub)
+#     print("result", ave)
+
+# result(10,20)
+# result(13,23,12,21,34)
+# result(576,7,4343,2)
+
+
+######################
+
+# def getdata(**data):
+#     print(data)
+
+
+# getdata(id=101, salary = 000, location= "pune", name = "Rupesh")
+
+######################
+
+L1 = [3, 4, 7, 2, 4, 6, 8, 5, 9, 3]
+
+index = 0
+row = 1
+
+for i in range(1, 5): 
+
+    for j in range(i):
+        print(L1[index], end=" ")
+        index += 1
+    print()
+
+max = []
+max.append(row)
+print(max)
+
+
+
+
+
